c2d24196cc75ece876dedc87ecbd00c5b38d274f
aigizshai <aigizshai@gmail.com>
2025-04-20 23:35:38 +0400

    feat: LAB1 Добавлена swagger-документация

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,46 @@ import { authDB, syncDB } from './configs/db.js'
 import eventsRouter from './routes/events.js'
 import userRouter from './routes/users.js'
 import cors from 'cors'
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import { readFileSync } from 'fs'
+import path, { join } from 'path'
 
-const port = process.env.APP_PORT
 
+const port = process.env.APP_PORT 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+const loadYAML = (file) => {
+    return readFileSync(join(process.cwd(), `swagger/${file}.yml`), 'utf-8')
+}
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API документация',
+            version: '1.0.0',
+        },
+        servers: [{url: 'http://localhost:9000',},],
+        components: loadYAML("schemas").components,
+        paths: {
+            ...loadYAML("events").paths,
+            ...loadYAML("users").paths,
+        }
+    },
+    apis: ["./swagger/*.yml"],
+}
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions)
+
+
+
+
 app.use("/events",eventsRouter)
 app.use("/users",userRouter)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.get('/', (req, res) => {
     res.send('API работает!')
 })  
